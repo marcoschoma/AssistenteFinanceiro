60eaaf53d1b78b1dca9998ca530ef9f2b3248dec
marcos choma <marcos.choma@brasilplural.com>
2018-03-16 19:41:47 -0300

    Initial commit

---
+++ AF.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AF.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ AF.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AF.Domain.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
