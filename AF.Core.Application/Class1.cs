cb74ce14e28914988f0a01855c6ab78811c664bc
Marcos Choma <marcos.choma@gmail.com>
2018-11-20 20:38:11 -0200

    Movendo arquivos

---
+++ AF.Core.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AF.Core.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ AF.Core.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AF.Core.Domain
+{
+    public class Class1
+    {
+    }
+}
